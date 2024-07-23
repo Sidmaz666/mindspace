3c2dd0d4f717c3b971e845b8755c2640d1f5b760
Sid <sidmazumder8@gmail.com>
2024-07-23 16:45:36 +0530

    add static img route

--- index.mjs
+++ index.mjs
@@ -30,7 +30,7 @@ const backlog = () => {
 }
 
 app.use(express.static(path.join(__dirname,"client")))
-app.use(express.static('./client/img'))
+app.use("/img", express.static(path.join(__dirname,"img")))
 
 app.get("/", (req,res) => {
   res.status(200).sendFile(path.join(__dirname,"client","index.html"))
